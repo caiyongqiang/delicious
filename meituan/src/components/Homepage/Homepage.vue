697a6f28bd167b74f6cf299fb5f44afb9d02678c
caiyongqiang <1324019229@qq.com>
2017-11-18 19:19:17 +0800

    电影

--- meituan/src/components/Homepage/Homepage.vue
+++ meituan/src/components/Homepage/Homepage.vue
@@ -2,6 +2,7 @@
   <div class="home">
   	<my-nav></my-nav>
   	测试home
+  <img src="http://maoyan.meituan.net/movie/videos/854x480b72a4617ae6944c6b4d7472818cf0e4b.mp4">
   </div>
 </template>
 
@@ -12,11 +13,23 @@ import axios from 'axios';
 export default {
   data () {
     return {
-     
+     num:3,
+     nums:1,
+     num1:1,
+     scor:[]
     }
   },
   created(){
-    
+    axios.get('http://xlcyq.sc2yun.com/data/data.php',{
+    params: {
+      file: 'https://m.maoyan.com/movie/1196171.json'}}).then
+    (res =>{
+      console.log(res);
+    });
+   
+  },
+  methods:{
+
   },
   components:{
   	'my-nav':Nav
--- meituan/src/router/index.js
+++ meituan/src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 
 import Homepage from '@/components/Homepage/Homepage'
 import Find from '@/components/Find/Find'
+import Details from '@/components/Find/Details/Details'
 import Order from '@/components/Order/Order'
 import My from '@/components/My/My'
 
@@ -20,6 +21,10 @@ export default new Router({
     {
       path: '/find',
       component: Find
+    },
+    {
+      path:'/details',
+      component:Details
     },
     {
       path: '/order',
